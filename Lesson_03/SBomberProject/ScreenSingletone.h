ead3a83628cd07c353888faf139916ab44070acf
accgh4gb <accgh4gb@gmail.com>
2023-01-29 22:21:34 +0600

    The first task of the third lesson

---
+++ Lesson_03/SBomberProject/ScreenSingletone.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+// Палитра цветов от 0 до 15
+enum ConsoleColor
+{
+    CC_Black = 0,
+    CC_Blue,
+    CC_Green,
+    CC_Cyan,
+    CC_Red,
+    CC_Magenta,
+    CC_Brown,
+    CC_LightGray,
+    CC_DarkGray,
+    CC_LightBlue,
+    CC_LightGreen,
+    CC_LightCyan,
+    CC_LightRed,
+    CC_LightMagenta,
+    CC_Yellow,
+    CC_White
+};
+
+class ScreenSingletone
+{
+public:
+    static ScreenSingletone& getInstance()
+    {
+        static ScreenSingletone theInstance;
+        return theInstance;
+    }
+    void ClrScr();
+    void __fastcall GotoXY(double x, double y);
+    uint16_t GetMaxX();
+    uint16_t GetMaxY();
+    void SetColor(ConsoleColor color);
+
+private:
+    ScreenSingletone() { }
+    ScreenSingletone(const ScreenSingletone& root) = delete;
+    ScreenSingletone& operator=(const ScreenSingletone&) = delete;
+};
